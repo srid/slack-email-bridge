6cf2004f61f296a8fbaa0ff16bf63825c72b1198
Sridhar Ratnakumar <srid@TinyBook.local>
2016-08-16 10:44:25 -0400

    Initial commit

---
+++ lib/slack_email_bridge.ex
@@ -0,0 +1,2 @@
+defmodule SlackEmailBridge do
+end
---
+++ test/slack_email_bridge_test.exs
@@ -0,0 +1,8 @@
+defmodule SlackEmailBridgeTest do
+  use ExUnit.Case
+  doctest SlackEmailBridge
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
